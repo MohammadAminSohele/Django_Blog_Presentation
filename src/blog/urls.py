94c2e5f8aa19057425cafb4cdac5cd682d7252a0
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2025-03-21 02:48:05 +0330

    modified: src/blog/urls.py modified: src/templates/home_page.html

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .import views
 
+app_name='blog'
+
 urlpatterns = [
     path('',views.ArticleList.as_view(),name='home_page'),
     path('<slug:slug>',views.ArticleDetail.as_view(),name='ArticleDetail'),
