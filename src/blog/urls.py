0ed339ec343cb17a348908a01fc7d7c1a1757303
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2025-03-21 03:16:14 +0330

    add AuthorList class

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,5 +6,6 @@ app_name='blog'
 
 urlpatterns = [
     path('',views.ArticleList.as_view(),name='home_page'),
+    path('<slug:username>',views.AuthorList.as_view(),name='AuthorList'),
     path('<slug:slug>',views.ArticleDetail.as_view(),name='ArticleDetail'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView,DetailView
 
 from .models import Article
+from account.models import User
 
 # Create your views here.
 
+class AuthorList(ListView):
+    paginate_by=3
+    template_name='blog/Show_articles_by_author.html' 
+    def get_queryset(self):
+        global author
+        username=self.kwargs.get('username')
+        author=get_object_or_404(User,username=username)
+        return author.articles.published()
+    def get_context_data(self, **kwargs) -> dict[str]:
+        context = super().get_context_data(**kwargs)
+        context["author"] = author
+        return context
+    
 class ArticleList(ListView):
     paginate_by=3
     template_name='home_page.html'
@@ -21,3 +35,4 @@ class ArticleDetail(DetailView):
     def get_object(self):
         slug=self.kwargs.get('slug')
         return get_object_or_404(Article.objects.published(),slug=slug)
+    
