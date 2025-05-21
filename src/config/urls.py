873aaf4a94cf1cf72e254a95347e5d4b7e6be99c
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2025-05-21 21:36:13 +0330

    add Ecomrce app

--- src/config/urls.py
+++ src/config/urls.py
@@ -25,6 +25,7 @@ from account.views import Login, LogoutView , Register , activate
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('comment/', include('comment.urls')),
+    path('ecomerce/', include('ecomerce.urls')),
 ]
 
 urlpatterns+=[
